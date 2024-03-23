3f34383c644bb0abccbe603b95a06b212709b25d
haansah1 <ansahhubert8588@gmail.com>
2024-03-23 10:50:32 +0000

    Deploying to Github

--- dsa_project/settings.py
+++ dsa_project/settings.py
@@ -79,14 +79,14 @@ DATABASES = {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'postgres',
-    #     'USER': 'postgres.vjogpmtxbxqgviyqmbxs',
-    #     'PASSWORD': 'A-3152128-Zf',
-    #     'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
-    #     'PORT': '5432',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.quzbcitrviaxwmhcpfde',
+        'PASSWORD': 'A-3152128-Zf',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+        'PORT': '5432',
+    }
 }
 
 
