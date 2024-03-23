3d187648b6ee0a650293a13cfd3e2169df4808bd
haansah1 <ansahhubert8588@gmail.com>
2024-03-23 18:31:22 +0000

    Deploying to Github

--- dsa_project/settings.py
+++ dsa_project/settings.py
@@ -129,9 +129,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = 'uploads/article.txt/'
+MEDIA_URL = '/uploads/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "uploads/article.txt/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
